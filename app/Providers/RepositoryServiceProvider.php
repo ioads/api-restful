e0e8d477198a1f58005a45ec4ba96aedd6479dd7
Iago Oliveira <ioads@outlook.com>
2024-06-17 15:18:01 -0300

    adiciona repository no provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\PlayerRepository;
+use App\Repositories\PlayerRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\TeamRepository;
 use App\Repositories\TeamRepositoryInterface;
@@ -11,5 +13,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(TeamRepositoryInterface::class, TeamRepository::class);
+        $this->app->bind(PlayerRepositoryInterface::class, PlayerRepository::class);
     }
 }
