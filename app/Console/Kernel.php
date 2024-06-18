1bdb4c1e07087d4d747e7ecd89e17e69708268a7
Iago Oliveira <ioads@outlook.com>
2024-06-17 22:17:52 -0300

    adiciona os commands no schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:create-teams-command')->daily();
+        $schedule->command('app:create-players-command')->daily();
+        $schedule->command('app:create-games-command')->daily();
     }
 
     /**
